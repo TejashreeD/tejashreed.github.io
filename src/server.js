3c313bb15bc168fdf6aa8edfbf42d5a72604f832
Tejashree Deshmukh <Tejashree.Deshmukh@blueconchtech.com>
2021-02-22 11:09:08 +0530

    heroku 1

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/sr-portfolio'));
+app.use(express.static(__dirname + '/dist/sr-portfolio'));
 
 app.get("/*", function (req, res) {
     // res.sendFile('index.html', {
